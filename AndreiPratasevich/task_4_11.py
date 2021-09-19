8b8fb9f9c4b3a970a5bd803609507224ad3cb9db
A.Protasevich <larsplotner@gmail.com>
2021-09-19 19:15:00 +0300

    finish task

--- AndreiPratasevich/task_4_11.py
+++ AndreiPratasevich/task_4_11.py
@@ -19,3 +19,19 @@ print(combine_dicts(dict_1, dict_2, dict_3)
 ```
 """
 
+def combine_dicts(*args):
+    d = {}
+
+    for item in args:
+        for key in item.keys():
+            d[key] = d.get(key,0) + item[key]
+    
+    return d
+
+
+dict_1 = {'a': 100, 'b': 200}
+dict_2 = {'a': 200, 'c': 300}
+dict_3 = {'a': 300, 'd': 100}
+
+print(combine_dicts(dict_1, dict_2))
+print(combine_dicts(dict_1, dict_2, dict_3))
