6e4aa70eb10473fa8d21b44db4e097b17afa0fbb
A.Protasevich <larsplotner@gmail.com>
2021-09-19 19:03:50 +0300

    start task

---
+++ AndreiPratasevich/task_4_10.py
@@ -0,0 +1,8 @@
+"""
+Implement a function that takes a number as an argument and returns a dictionary, where the key is a number and the value is the square of that number.
+```python
+print(generate_squares(5))
+>>> {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+```
+"""
+
