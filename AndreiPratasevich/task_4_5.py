134d9546abca7b4a7f00dc2dc922b362eeb8c67c
A.Protasevich <larsplotner@gmail.com>
2021-09-19 13:33:52 +0300

    start task

---
+++ AndreiPratasevich/task_4_5.py
@@ -0,0 +1,9 @@
+"""
+Implement a function `get_digits(num: int) -> Tuple[int]` which returns a tuple
+of a given integer's digits.
+Example:
+```python
+>>> split_by_index(87178291199)
+(8, 7, 1, 7, 8, 2, 9, 1, 1, 9, 9)
+```
+"""
