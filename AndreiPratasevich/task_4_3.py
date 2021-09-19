7578f69960533d34ad126cd6603a12fe27a8f184
A.Protasevich <larsplotner@gmail.com>
2021-09-19 12:59:20 +0300

    finish task

--- AndreiPratasevich/task_4_3.py
+++ AndreiPratasevich/task_4_3.py
@@ -1,4 +1,44 @@
-def split_string(s):
-    pass
+def split_string(string, sep=' ', max_split = -1):
+    """Not accurate, not fast but work"""
+    out = [] # resulting list
 
-test = ['hello Moto', '1,2,3', '3 2 1']
+    # find all split indexes
+    split_indexs_all = [0]
+    for i in range(len(string)):
+        if string[i] == sep:
+            split_indexs_all.append(i)
+    
+    split_indexs_all.append(len(string)) # add last index
+
+# if max split set get only valid number of splits
+    if max_split > 0:
+        split_indexs = []
+        split_indexs.append(split_indexs_all[0])
+        
+        for ind in split_indexs_all[1:1+max_split]:
+            split_indexs.append(ind)
+        
+        split_indexs.append(split_indexs_all[-1])
+    else:
+        split_indexs = split_indexs_all
+
+    for j in range(len(split_indexs)-1):
+        if j == 0:
+            out.append(string[split_indexs[j]:split_indexs[j+1]])
+        else:
+            out.append(string[split_indexs[j]+1:split_indexs[j+1]])
+
+    
+    return out
+
+    
+
+
+tests = ['hello Moto', '1,2,3,4,5,6,7', '4 3 2 1']
+
+for test in tests:
+    print(split_string(test))
+
+print(split_string(tests[1],sep=','))
+
+print(split_string(tests[1],sep=',', max_split=2))
