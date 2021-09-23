b5eda6d013c2fb3386cf810c63d4039109ea16b2
A.Protasevich <larsplotner@gmail.com>
2021-09-23 20:21:33 +0300

    finish task

--- AndreiPratasevich/task_5_3.py
+++ AndreiPratasevich/task_5_3.py
@@ -18,4 +18,26 @@ def get_top_performers(file_path, number_of_top_students=5):
     
     return top_names
 
-print(get_top_performers(data_path))
+# print(get_top_performers(data_path))
+
+def sort_to_csv(file_path):
+    with open(file_path, 'r') as data:
+        stud_data = {}
+        vals = data.readline()
+        for line in data.readlines():
+            stud = {}
+            stud['age'] = int(line.split(',')[1])
+            stud['avg'] = float(line.split(',')[2][:-1])
+            name = line.split(',')[0]
+            stud_data[name] = stud
+
+    sorted_studs = dict(sorted(stud_data.items(), key = lambda x:x[1]['age'], reverse=True))
+    
+    with open('task_5_3.txt', 'a') as out_file:
+        out_file.write(vals)
+        for key in sorted_studs.keys():
+            stud = sorted_studs[key]
+            out_file.write(f'{key},{stud["age"]},{stud["avg"]}\n')
+
+
+print(sort_to_csv(data_path))
