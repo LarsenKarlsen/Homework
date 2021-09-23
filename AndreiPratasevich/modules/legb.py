faf9911388072cb7231572b64af29c894b3772a9
A.Protasevich <larsplotner@gmail.com>
2021-09-23 20:58:19 +0300

    task 5_4 done

--- AndreiPratasevich/modules/legb.py
+++ AndreiPratasevich/modules/legb.py
@@ -5,6 +5,11 @@ def enclosing_funcion():
     a = "I am variable from enclosed function!"
 
     def inner_function():
-
+        print(globals()['a'])
+        nonlocal a; print(a)
         a = "I am local variable!"
         print(a)
+    return inner_function
+
+x = enclosing_funcion()
+x()
