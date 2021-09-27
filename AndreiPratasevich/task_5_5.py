03c63d26dcd872903abbca4a19656d50776d9cc3
A.Protasevich <larsplotner@gmail.com>
2021-09-27 20:49:45 +0300

    task finished

--- AndreiPratasevich/task_5_5.py
+++ AndreiPratasevich/task_5_5.py
@@ -21,3 +21,24 @@ sum_list(3, 4, 5)
 >>> "Current result = '12'"
 ```
 """
+def remember_result(func):
+    def wrapper(*args, prev=[None]):
+        print(f"Last result = '{prev[-1]}'")
+        prev.append(func(*args))
+    return wrapper
+
+@remember_result
+def sum_list(*args):
+    if isinstance(args[0], int):
+        result = 0
+    else:
+	    result = ""
+
+    for item in args:
+	    result += item
+    print(f"Current result = '{result}'")
+    return result
+
+sum_list("a", "b")
+sum_list("abc", "cde")
+sum_list(3, 4, 5)
